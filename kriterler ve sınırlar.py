616245ae0f3f4e1def89a9cf24d1a8e94c65b627
Müberra KOÇAK <80243731+MuberraKocak@users.noreply.github.com>
2021-05-21 15:19:56 +0300

    Update kriterler ve sınırlar.py

--- kriterler ve sınırlar.py
+++ kriterler ve sınırlar.py
@@ -1,2 +1,6 @@
+#Bahsedilen kriterlerin sınırlarını belirleyeceğiz.
+#TC no ile başvurulan sistemden yukarıda bahsedilen kriterlere ait bilgileri sistemden çekeceğiz.
+#Aday bilgilerini dosyaya kaydedeceğiz. (her aday için bir aday/başvuru no atanacak)
+
 TCno=int(input("TC No: "))
 
