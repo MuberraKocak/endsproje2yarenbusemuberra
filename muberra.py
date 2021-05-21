7395a033a8226ed236fd2da607c7b252557769ec
Müberra KOÇAK <80243731+MuberraKocak@users.noreply.github.com>
2021-05-21 17:52:57 +0300

    Update muberra.py

--- muberra.py
+++ muberra.py
@@ -1,3 +1,5 @@
+#MÜBERRA KOÇAK
+
 #Bahsedilen kriterlerin sınırlarını belirleyeceğiz.
 #TC no ile başvurulan sistemden yukarıda bahsedilen kriterlere ait bilgileri sistemden çekeceğiz.
 #Aday bilgilerini dosyaya kaydedeceğiz. (her aday için bir aday/başvuru no atanacak)
