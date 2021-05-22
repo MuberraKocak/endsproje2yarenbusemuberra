523b0c25e7c9d37da1569de270551fcb0c578cf3
Müberra KOÇAK <80243731+MuberraKocak@users.noreply.github.com>
2021-05-22 23:13:59 +0300

    Update muberra.py

--- muberra.py
+++ muberra.py
@@ -6,14 +6,6 @@
 #Her adayın puanını hesaplayacağız.
 #and, or, if, elif, else kullanarak değerlendirme yapacağız.
 
-#MÜBERRA KOÇAK
-
-#Bahsedilen kriterlerin sınırlarını belirleyeceğiz.
-#TC no ile başvurulan sistemden yukarıda bahsedilen kriterlere ait bilgileri sistemden çekeceğiz.
-#Aday bilgilerini dosyaya kaydedeceğiz. (her aday için bir aday/başvuru no atanacak)
-#Her adayın puanını hesaplayacağız.
-#and, or, if, elif, else kullanarak değerlendirme yapacağız.
-
 #yeterlilik alan göstergeleri
 akademik_mesleki_yeterlilikler =  ("genel not ortalaması", "Bilimsel projelerden/yarışmalardan ödül kazandınız mı?", "ingilizce seviyenizi sayısal olarak belirtiniz (1:Yok, 2:Başlangıç, 3:Orta, 4:İyi, 5:Çok İyi): ")
 notOrtAgirlik = 0.50
@@ -27,18 +19,16 @@ sosyalOdulAgirlik = 0.2
 sportif_yeterlilikler = ("profesyonel sporcu lisansına sahip olmak")
 sportifOdulAgirlik = 0.2
 
-#toplam ağırlıklı puan (tap) hesaplaması
-
-#ağırlıklı toplam seçme puanı (atsp) hesaplanması
+#ağırlıklı toplam seçme puanı (atsp) hesaplanması......................................................................
 
 #kullanıcıdan bilgilerin alınması ve puan hesaplamaları
 aday=1
 while aday<=10:
-  TCno=int(input("TC No: "))
-  isim=input("isim: ")
-  soyisim=input("soyisim: ")
+  TCno=int(input("TC kimlik numaranızı giriniz: "))
+  isim=input("İsminiz: ")
+  soyisim=input("Soyisminiz: ")
 
-  notOrt=float(input("genel not ortalamanızı giriniz: "))
+  notOrt=float(input("Genel not ortalamanızı giriniz: "))
   if (notOrt > 3.49 and notOrt <= 4.00) or (notOrt > 87.25 and notOrt <= 100.00):
     notOrtPuan = 100
   elif (notOrt > 2.99 and notOrt <= 3.49) or (notOrt > 74.75 and notOrt <= 87.25):
@@ -51,14 +41,16 @@ while aday<=10:
     notOrtPuan = 20
   else:
     print("Lütfen girdiğiniz not ortalamasını kontrol edip tekrar giriniz!")
-  print(f'norOrtPuan={notOrtPuan}')
+  print(f'Genel not ortalaması puanı={notOrtPuan}')
   
-  bilimselOdul = bool(input("bilimsel projelerden/yarışmalardan ödül aldınız mı?: "))
-  if (bilimselOdul == "Evet"):
-    bilimselOdulPuan=100
-  else:
+  bilimselOdul = (input("Bilimsel projelerden/yarışmalardan ödül aldınız mı?: "))
+  if (bilimselOdul == "evet"):
+    bilimselOdulPuan = 100
+  elif (bilimselOdul == "hayır"):
     bilimselOdulPuan = 0
-  print(f'bilimselOdulDogrulamaPuan={bilimselOdulPuan}')
+  else:
+    print("Lütfen girdiğiniz bilgiyi kontrol edip tekrar giriniz!")  
+  print(f'Bilimsel ödül puanı={bilimselOdulPuan}')
   
   ingilizceSeviyesi = int(input("ingilizce seviyenizi sayısal olarak belirtiniz (1:Yok, 2:Başlangıç, 3:Orta, 4:İyi, 5:Çok İyi): "))
   if ingilizceSeviyesi == 5:
@@ -73,22 +65,26 @@ while aday<=10:
     ingilizceSeviyesiPuan = 20
   elif ingilizceSeviyesi == 0:
     ingilizceSeviyesiPuan = 0
-  print(f'ingilizceSeviyesiPuan={ingilizceSeviyesiPuan}')
-  akademik_mesleki_yeterlilikTAP=(notOrtAgirlik*notOrt)+(bilimselOdulAgirlik*bilimselOdul)+(ingilizceSeviyesiAgirlik*ingilizceSeviyesiPuan)
-  
-  sosyalOdul = bool(input("sosyal projelerden/yarışmalardan ödül aldınız mı?: "))
-  if sosyalOdul == True:
+  print(f'İngilizce seviyesi puanı = {ingilizceSeviyesiPuan}')
+  akademik_mesleki_yeterlilikTAP = (notOrtAgirlik*notOrtPuan)+(bilimselOdulAgirlik*bilimselOdulPuan)+(ingilizceSeviyesiAgirlik*ingilizceSeviyesiPuan)
+  print(f'Akademik/Mesleki Yeterlilik Puanı = {akademik_mesleki_yeterlilikTAP}')
+
+  sosyalOdul = (input("Sosyal projelerden/yarışmalardan ödül aldınız mı?: "))
+  if sosyalOdul == "evet":
     sosyalOdulPuan = 100
   else:
     sosyalOdulPuan = 0
+  sanatsal_sosyal_yeterlilikTAP=(sosyalOdulAgirlik*sosyalOdulPuan)
+  print(f'Sanatsal/Sosyal Yeterlilik Puanı = {sanatsal_sosyal_yeterlilikTAP}')
 
-  sportifOdul = bool(input("profesyonel sporcu lisansınız bulunuyor mu?: "))
-  if sportifOdul == True:
+  sportifOdul = (input("Profesyonel sporcu lisansınız bulunuyor mu?: "))
+  if sportifOdul == "evet":
     sportifOdulPuan = 100
   else:
     sportifOdulPuan = 0
+  sportif_yeterlilikTAP=(sportifOdulAgirlik*sportifOdulPuan)
+  print(f'Sportif Yeterlilik Puanı = {sportif_yeterlilikTAP}')
 
-  print(f'{aday} aday TC No: {TCno} isim: {isim} soyisim: {soyisim}')
-  #, akademik mesleki yeterlilik puanı={akademik_mesleki_yeterlilikTAP}, sanatsal sosyal yeterlilik puanı={sanatsal_sosyal_yeterlilikTAP}, sportif yeterlilik puanı={sportif_yeterlilikTAP}
+  print(f'{aday} aday TC No: {TCno} isim: {isim} soyisim: {soyisim}, akademik mesleki yeterlilik puanı={akademik_mesleki_yeterlilikTAP}, sanatsal sosyal yeterlilik puanı={sanatsal_sosyal_yeterlilikTAP}, sportif yeterlilik puanı={sportif_yeterlilikTAP}')
 
 aday +=1
